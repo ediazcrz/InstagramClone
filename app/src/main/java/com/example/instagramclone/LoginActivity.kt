2c854dcc1383809fa67643b7f8e2a87dea0aa271
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-20 18:19:42 -0500

    logs in user if they are not already logged in

--- app/src/main/java/com/example/instagramclone/LoginActivity.kt
+++ app/src/main/java/com/example/instagramclone/LoginActivity.kt
@@ -13,8 +13,48 @@ import com.parse.ParseUser
 const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+
+        if (ParseUser.getCurrentUser() != null) {
+            launchMainActivity()
+        }
+
+        val etUsername = loginBinding.etUsername
+        val etPassword = loginBinding.etPassword
+        val btnLogin = loginBinding.btnLogin
+
+        btnLogin.setOnClickListener() {
+            Log.i(TAG, "onClick login button")
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+            loginUser(username, password)
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        Log.i(TAG, "Attempting to login user $username")
+        ParseUser.logInInBackground(username, password, object: LogInCallback {
+            override fun done(user: ParseUser?, e: ParseException?) {
+                if (e!= null) {
+                    Log.e(TAG,"Issue with login", e)
+                    return
+                }
+
+                // navigate to the main activity if the user has signed in properly
+                launchMainActivity()
+                Toast.makeText(this@LoginActivity, "Success!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun launchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }

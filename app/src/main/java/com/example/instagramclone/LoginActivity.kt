39f24a4a7b495b386d092d46a0a7a5db413ad957
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-20 18:19:22 -0500

    necessary imports

--- app/src/main/java/com/example/instagramclone/LoginActivity.kt
+++ app/src/main/java/com/example/instagramclone/LoginActivity.kt
@@ -1,7 +1,16 @@
 package com.example.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.instagramclone.databinding.ActivityLoginBinding
+import com.parse.LogInCallback
+import com.parse.ParseException
+import com.parse.ParseUser
+
+const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

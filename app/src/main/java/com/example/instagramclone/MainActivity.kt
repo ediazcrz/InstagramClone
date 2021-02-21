1b762dfd86e21033ad38ef4023a488094f0c8ef3
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-20 22:45:04 -0500

    defined private class variables

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var etDescription: EditText
+    private lateinit var photoFile: File
+    private lateinit var ivPostImage: ImageView
+    private val photoFileName: String = "photo.jpg"
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

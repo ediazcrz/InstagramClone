9e801601419f3f154f5da0258dd686a3b275db6d
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-20 22:51:20 -0500

    defines a Post Parse object

---
+++ app/src/main/java/com/example/instagramclone/Parse.kt
@@ -0,0 +1,28 @@
+package com.example.instagramclone
+
+import com.parse.ParseClassName
+import com.parse.ParseFile
+import com.parse.ParseObject
+import com.parse.ParseUser
+import com.parse.ktx.putOrIgnore
+
+@ParseClassName("Post")
+class Post : ParseObject() {
+    companion object {
+        const val KEY_DESCRIPTION = "description"
+        const val KEY_IMAGE = "image"
+        const val KEY_USER = "user"
+    }
+
+    var description : String?
+        get() = getString(KEY_DESCRIPTION)
+        set(value) = putOrIgnore(KEY_DESCRIPTION, value)
+
+    var image : ParseFile?
+        get() = getParseFile(KEY_IMAGE)
+        set(value) = putOrIgnore(KEY_IMAGE, value)
+
+    var user : ParseUser?
+        get() = getParseUser(KEY_USER)
+        set(value) = putOrIgnore(KEY_USER, value)
+}

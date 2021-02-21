3471fe89365b4a388a3a3870b15b5b73fefe7702
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-20 22:46:38 -0500

    registers the Post parse object

--- app/src/main/java/com/example/instagramclone/ParseApplication.kt
+++ app/src/main/java/com/example/instagramclone/ParseApplication.kt
@@ -2,13 +2,16 @@ package com.example.instagramclone
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
 
 class ParseApplication: Application() {
-    // Initializes Parse SDK as soon as the application is created
     override fun onCreate() {
         super.onCreate()
 
         // Register parse models
+        ParseObject.registerSubclass(Post::class.java)
+
+        // Initializes Parse SDK as soon as the application is created
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(BuildConfig.APPLICATION_ID)

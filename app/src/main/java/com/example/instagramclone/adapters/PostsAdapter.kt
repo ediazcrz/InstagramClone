4eae61b1a945492da6626bdfbb17278f698c08ba
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-21 21:04:05 -0500

    defines an adapter for Posts

---
+++ app/src/main/java/com/example/instagramclone/adapters/PostsAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.instagramclone.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.bumptech.glide.Glide
+import com.example.instagramclone.Post
+import com.example.instagramclone.adapters.PostsAdapter.PostsViewHolder
+import com.example.instagramclone.databinding.ItemPostBinding
+
+class PostsAdapter(private val context: Context, private val posts: ArrayList<Post>): Adapter<PostsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
+        val view = ItemPostBinding.inflate(LayoutInflater.from(context), parent, false)
+        return PostsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
+        val post = posts[position]
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    inner class PostsViewHolder(private val binding: ItemPostBinding): ViewHolder(binding.root) {
+        fun bind(post: Post) {
+            binding.tvUsername.text = post.user?.username
+            binding.tvDescription.text = post.description
+            Glide.with(context).load(post.image?.url).into(binding.ivImage)
+        }
+    }
+
+}

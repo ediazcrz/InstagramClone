21c974da32a51e0621767386220c9c76bbed589f
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-28 20:58:10 -0500

    optimized imports

--- app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.kt
@@ -1,18 +1,7 @@
 package com.example.instagramclone.fragments
 
-import android.content.Intent
-import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import com.example.instagramclone.Post
-import com.example.instagramclone.R
-import com.example.instagramclone.activities.LoginActivity
-import com.example.instagramclone.databinding.FragmentProfileBinding
-import com.parse.ParseException
 import com.parse.ParseQuery
 import com.parse.ParseUser
 

6464d08e2e9f1292da9c2efdce1fbe45b293d2ff
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-28 20:53:50 -0500

    inherits from the PostsFragement class

--- app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.kt
@@ -8,45 +8,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.example.instagramclone.Post
 import com.example.instagramclone.R
 import com.example.instagramclone.activities.LoginActivity
 import com.example.instagramclone.databinding.FragmentProfileBinding
 import com.parse.ParseException
+import com.parse.ParseQuery
 import com.parse.ParseUser
 
 private const val TAG = "ProfileFragment"
 
-class ProfileFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val profileBinding = FragmentProfileBinding.bind(view)
-        val btnLogOut = profileBinding.btnLogOut
-
-        btnLogOut.setOnClickListener {
-            val currentUser = ParseUser.getCurrentUser()
-            ParseUser.logOutInBackground { e: ParseException? ->
-                if (e == null) {
-                    Log.i(TAG, "Logged out ${currentUser.username}")
-                    Toast.makeText(context, "You are now signed out", Toast.LENGTH_SHORT).show()
-                    launchLoginActivity()
-                } else {
-                    Log.e("TAG", "Error logging out", e)
+class ProfileFragment : PostsFragment() {
+    override fun queryPosts() {
+        val query = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser())
+        query.limit = 20
+        query.addDescendingOrder(Post.KEY_CREATED_AT)
+        query.findInBackground { posts, e ->
+            if (posts != null) {
+                for (post in posts) {
+                    Log.i(TAG, "Post: ${post.description}, username: ${post.user?.username}")
                 }
+                allPosts.addAll(posts)
+                postsAdapter.notifyDataSetChanged()
+            } else {
+                Log.e(TAG, "Issues with getting posts", e)
             }
         }
-    }
-
-    private fun launchLoginActivity() {
-        val intent = Intent(context, LoginActivity::class.java)
-        startActivity(intent)
     }
 }

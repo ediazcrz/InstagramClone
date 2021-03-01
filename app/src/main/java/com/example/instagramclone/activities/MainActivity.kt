89fcfd9e85297409f01d815c6ce6a97e8dae5cb6
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-28 20:52:20 -0500

    listens for top navigation click

--- app/src/main/java/com/example/instagramclone/activities/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.instagramclone.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.instagramclone.R
@@ -8,6 +11,10 @@ import com.example.instagramclone.databinding.ActivityMainBinding
 import com.example.instagramclone.fragments.ComposeFragment
 import com.example.instagramclone.fragments.PostsFragment
 import com.example.instagramclone.fragments.ProfileFragment
+import com.parse.ParseUser
+import java.text.ParseException
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
@@ -18,8 +25,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
 
         val fragmentManager = supportFragmentManager
-
         val bottomNavView = mainBinding.bottomNavigation
+        val topAppBar = mainBinding.topAppBar
+
+        topAppBar.setOnMenuItemClickListener{ menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_log_out -> {
+                    val currentUser = ParseUser.getCurrentUser()
+                    ParseUser.logOutInBackground { e: com.parse.ParseException? ->
+                        if (e == null) {
+                            Log.i(TAG, "Logged out ${currentUser.username}")
+                            Toast.makeText(this, "You are now signed out", Toast.LENGTH_SHORT).show()
+                            launchLoginActivity()
+                        } else {
+                            Log.e(TAG, "Error logging out", e)
+                        }
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+                else -> false
+            }
+        }
 
         bottomNavView.setOnNavigationItemSelectedListener{ menuItem ->
             var fragment: Fragment? = null
@@ -35,5 +61,10 @@ class MainActivity : AppCompatActivity() {
         }
         // Set default selection
         bottomNavView.selectedItemId = R.id.action_home;
+    }
+
+    private fun launchLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
